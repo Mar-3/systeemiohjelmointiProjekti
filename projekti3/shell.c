dc7cc9cc32b5e3953548d318b956e87602bffa96
Mar-3 <markojutila1@gmail.com>
2022-07-31 14:17:14 +0300

    fixed memory leaks and bugs

--- projekti3/shell.c
+++ projekti3/shell.c
@@ -7,48 +7,54 @@
 
 #define MAX 60
 
-int runProgram(char *programAndArgs[MAX])
+void freeMemory(char *array[MAX])
 {
+    for (int i = 0; array[i] != NULL; i++)
+    {
+        free(array[i]);
+        array[i] = NULL;
+    }
+}
+
+int runProgram(char *programAndArgs[MAX], char *programPath) // This subprogram forks a new process and runs the
+{                                                            // wanted command with arguments in a new process.
+    char error_message[30] = "An error has occurred\n";
     int rc = fork();
     if (rc < 0)
     {
-        fprintf(stderr, "Fork failed\n");
+        write(STDERR_FILENO, error_message, strlen(error_message));
         return (1);
     }
     else if (rc == 0)
     {
-        execvp(programAndArgs[0], programAndArgs);
+        execvp(programPath, programAndArgs);
     }
     else
     {
+        free(programPath);
         return 0;
     }
+    free(programPath);
     return rc;
 }
 
 char *checkAccess(char *paths[MAX], char *programName)
 { // Subprogram for checking if program accessible
-    int counter = 0;
-    char *programPath;
+    char *programPath = (char *)malloc(sizeof(char) * MAX);
     char currentDirectory[MAX];
-    while (paths[counter] != NULL)
+    for (int i = 0; paths[i] != NULL; i++)
     { // Builds the possible paths for a program and checks access
-        programPath = strdup(paths[0]);
+        strcpy(programPath, paths[i]);
         strcat(programPath, "/");
         strcat(programPath, programName);
-
         if (access(programPath, X_OK) == 0)
         {
-            return programPath;
-        }
-        else
-        {
-            counter++;
+            return programPath; // Return path to program if accessible
         }
     } // checking for access in current directory
     if (getcwd(currentDirectory, sizeof(currentDirectory)) != NULL)
     {
-        programPath = strdup(currentDirectory);
+        strcpy(programPath, currentDirectory);
         strcat(programPath, "/");
         strcat(programPath, programName);
 
@@ -59,34 +65,33 @@ char *checkAccess(char *paths[MAX], char *programName)
             return programPath;
         }
     }
-    else
-    {
-        perror("cant get current folder.");
-    }
+    free(programPath);
     return NULL; // return NULL if not accessible
 }
 
 void tokenCommands(char *inputString, char *outputArray[MAX], char separator) // This subprogram is for separating the commands and argumets
 {
-    char *token;
+    char *token = NULL;
     int counter = 0;
     token = strtok(inputString, &separator);
     while (token != NULL)
-    {                                        // Iterating through inputstring and adding all the seperated string to outputarray
-        if (strcmp(token, strdup(" ")) != 0) // checking for stray spaces and not adding them to array
+    {                                // Goes through inputstring and adds all the seperated strings to an output array
+        if (strcmp(token, " ") != 0) // checking for stray spaces and not adding them to array
         {
             outputArray[counter] = strdup(token);
-            outputArray[counter + 1] = NULL;
             counter++;
         }
         token = strtok(NULL, &separator);
     }
-    outputArray[counter - 1] = strdup(strtok(outputArray[counter - 1], "\n"));
+    token = strtok(outputArray[counter - 1], "\n"); // Ignoring newline \n.
+    outputArray[counter - 1] = token;
+    outputArray[counter] = NULL;
     return;
 }
 
 int runOneLine(char *paths[MAX], char *line)
 {
+    char error_message[30] = "An error has occurred\n";
     int counter;
     char *programAndArgs[MAX];
     char *oneProgram[MAX];
@@ -105,40 +110,39 @@ int runOneLine(char *paths[MAX], char *line)
     while (programAndArgs[counter] != NULL)
     {
         tokenCommands(programAndArgs[counter], oneProgram, *" ");
-        if (!strcmp(oneProgram[0], "cd"))
+        if (strcmp(oneProgram[0], "cd") == 0) // if first command is cd change current directory
         {
             if ((chdir(oneProgram[1]) != 0) && (oneProgram[2] == NULL))
             {
-                printf("Error chdir");
-                exit(1);
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                break;
             }
         }
-        else if (!strcmp(oneProgram[0], "path"))
-        {
-            counter = 1;
-            while (oneProgram[counter] != NULL)
+        else if (strcmp(oneProgram[0], "path") == 0) // if first command is path, change the paths to
+        {                                            // search programs from.
+            freeMemory(paths);
+            for (int i = 1; oneProgram[i] != NULL; i++)
             {
-                paths[counter - 1] = strdup(oneProgram[counter]);
-                counter++;
+                paths[i - 1] = strdup(oneProgram[i]);
+                printf("%s \n", paths[i - 1]);
+                paths[i] = NULL;
             }
-            paths[counter] = NULL;
         }
         else
         {
             if ((programPath = checkAccess(paths, oneProgram[0])) == NULL)
             {
-                perror("Couldnt get access to program.");
+                printf("Couldnt get access to program.\n");
             }
             else
             {
                 for (int i = 0; oneProgram[i] != NULL; i++) // Used: https://stackoverflow.com/questions/584868/rerouting-stdin-and-stdout-from-c
-                {                                           // checking for file output sign ">" and redirecting stdin to output file if possible
-                    if (strcmp(oneProgram[i], ">") == 0)
+                {                                           // checking for file output sign ">" and redirecting stdout to output file if possible
+                    if ((strcmp(oneProgram[i], ">") == 0))
                     {
-
                         if ((freopen(oneProgram[i + 1], "w", stdout)) == NULL)
                         {
-                            perror("Cannot open output file");
+                            write(STDERR_FILENO, error_message, strlen(error_message));
                             exit(1);
                         }
                         free(oneProgram[i + 1]);
@@ -146,24 +150,31 @@ int runOneLine(char *paths[MAX], char *line)
                         oneProgram[i] = NULL;
                     }
                 }
-                if (runProgram(oneProgram) != 0)
+                strcpy(oneProgram[0], programPath);
+                if (runProgram(oneProgram, strdup(programPath)) != 0)
                 {
-                    perror("Problem running the program.");
+                    write(STDERR_FILENO, error_message, strlen(error_message));
                     exit(1);
                 }
             }
+            free(programPath);
         }
         counter++;
+        if (oneProgram[0] != NULL)
+        {
+            freeMemory(oneProgram);
+        }
     }
     while (wait(NULL) > 0) // waits for all the child processes to finish before
         ;                  // Giving control back to user.
 
-    // Setting the stdout back in case it was redirected to a file
+    // Setting the stdout back in case it was redirected to a file.
     freopen("/dev/tty", "w", stdout); // https://stackoverflow.com/questions/29154056/redirect-stdout-to-a-file
+    freeMemory(programAndArgs);
     return 0;
 }
 
-int interactiveShell()
+void interactiveShell()
 {
     // Help for using getline(): https://c-for-dummies.com/blog/?p=1112
     char *buffer;
@@ -175,17 +186,20 @@ int interactiveShell()
     paths[0] = strdup("/bin"); // Setting the default paths for programs
     paths[1] = strdup("/usr/bin");
     paths[2] = NULL;
-    while (stop == 0) //
+    while (stop == 0) // Interactive shells main loop for user input
     {
         printf("wish>");
         getline(&buffer, &bufferSize, stdin);
         stop = runOneLine(paths, buffer); // RunOneLine returns 1 when exit is typed, ending the loop.
     }
-    return 0;
+    freeMemory(paths);
+    free(buffer);
+    return;
 }
 
-int batchShell(char *fileName)
+void batchShell(char *fileName)
 {
+    char error_message[30] = "An error has occurred\n";
     char *paths[MAX];
     paths[0] = strdup("/bin"); // Setting the default paths for programs
     paths[1] = strdup("/usr/bin");
@@ -198,19 +212,21 @@ int batchShell(char *fileName)
 
     if ((file = fopen(fileName, "r")) == NULL)
     {
-        perror("Couldnt open file");
+        write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
     while (fgets(line, MAX, file) != NULL)
     {
         if (runOneLine(paths, line) == 1)
         {
-            perror("Error");
+            write(STDERR_FILENO, error_message, strlen(error_message));
             exit(1);
         }
     }
     fclose(file);
-    return 0;
+    freeMemory(paths);
+    free(line);
+    return;
 }
 
 int main(int argc, char **argv)
@@ -227,6 +243,5 @@ int main(int argc, char **argv)
         }
     }
     printf("Shell exiting successfully.\n");
-
-    return 0;
+    exit(0);
 }
